20ac201d15c03565a2af30b99381efca379ce1d5
mohan9638 <158275364+mohan9638@users.noreply.github.com>
2025-05-30 23:46:49 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,9 +6,9 @@ from os import environ
 API_ID = int(environ.get("API_ID", "28094744"))
 API_HASH = environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+OWNER = int(environ.get("OWNER", "5927517339"))
+CREDIT = "Astronaut"
+AUTH_USER = os.environ.get('AUTH_USERS', '5927517339').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
